02e7d36e0dd36393cb627fbdba2a089dadf7a5e0
spartan289 <60476934+spartan289@users.noreply.github.com>
2021-10-29 19:15:29 +0530

    make it better

--- intern/wsgi.py
+++ intern/wsgi.py
@@ -14,4 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intern.settings')
 
 application = get_wsgi_application()
-django_heroku.settings(locals())
